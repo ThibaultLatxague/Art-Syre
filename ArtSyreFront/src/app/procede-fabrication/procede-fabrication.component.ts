89c7b3f0794bd1128a13b298049f4dc0f9052b4b
Thibault Latxague <tlatxague@iutbayonne.univ-pau.fr>
2025-08-25 18:57:50 +0200

    Ajout du component procede de fabrication

---
+++ ArtSyreFront/src/app/procede-fabrication/procede-fabrication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-procede-fabrication',
+  standalone: false,
+  templateUrl: './procede-fabrication.component.html',
+  styleUrl: './procede-fabrication.component.scss'
+})
+export class ProcedeFabricationComponent {
+
+}
