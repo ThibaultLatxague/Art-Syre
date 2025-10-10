53557302aeae6f8db3dee667eb922a1e46211213
Thibault LATXAGUE <thibault.latxague@exakis-nelite.com>
2025-10-10 19:21:11 +0200

    Verification likes + souhait dans panier

--- ArtSyreFront/src/app/panier/panier.component.ts
+++ ArtSyreFront/src/app/panier/panier.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from '../models/utilisateur.model';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-panier',
@@ -6,6 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './panier.component.html',
   styleUrl: './panier.component.scss'
 })
-export class PanierComponent {
+export class PanierComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+  utilisateurCourant: Utilisateur | null = null;
 
+  ngOnInit(): void {
+    this.loadCurrentUser();
+    if(this.utilisateurCourant) {
+      this.chargerTableaux();
+    }
+  }
+
+  private loadCurrentUser(): void {
+    // Initialiser avec un utilisateur vide
+    this.utilisateurCourant = new Utilisateur(0, '', '', '', '', '', false, [], []);
+    
+    // Récupérer l'utilisateur courant
+    this.utilisateurCourant = this.authService.getCurrentUserAngular();
+    
+    console.log("Utilisateur courant dans le header:", this.utilisateurCourant);
+  }
+
+  private chargerTableaux(): void {
+    // Logique pour charger les tableaux de l'utilisateur
+    console.log("Charger les tableaux pour l'utilisateur:", this.utilisateurCourant);
+  }
 }
