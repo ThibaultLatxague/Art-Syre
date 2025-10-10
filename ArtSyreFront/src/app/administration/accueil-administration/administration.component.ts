35f9b254d8fba13679d52ccdf821ad9f4fe36b9f
Thibault LATXAGUE <thibault.latxague@exakis-nelite.com>
2025-10-10 20:12:26 +0200

    Mise a jour empty-state dans app.component.scss + ajout verif utilisateur admin logs

--- ArtSyreFront/src/app/administration/accueil-administration/administration.component.ts
+++ ArtSyreFront/src/app/administration/accueil-administration/administration.component.ts
@@ -6,7 +6,7 @@ import { Utilisateur } from '../../models/utilisateur.model';
   selector: 'app-administration',
   standalone: false,
   templateUrl: './administration.component.html',
-  styleUrl: './administration.component.scss'
+  styleUrls: ['./administration.component.scss', '../../app.component.scss']
 })
 export class AdministrationComponent implements OnInit {
   constructor(private authService: AuthService) {}
