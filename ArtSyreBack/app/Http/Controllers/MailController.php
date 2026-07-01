0408f70d70dcad6b0e59d5e53990da74463e7297
Thibault LATXAGUE <thibault.latxague@exakis-nelite.com>
2026-07-01 14:48:20 +0200

    feat: added mail controller, logic and blade in backend

---
+++ ArtSyreBack/app/Http/Controllers/MailController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Mail\ContactMail;
+
+class MailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function send(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'subject' => 'required|string',
+            'message' => 'required|string',
+        ]);
+
+        try{
+            \Mail::to('contact@monsite.test')->send(new \App\Mail\ContactMail($data));
+            return response()->json([
+                'success' => true,
+                'message' => 'Le mail a été envoyé avec succès.'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Erreur lors de l\'envoi du mail',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
---
+++ ArtSyreBack/resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<h2>Nouveau message</h2>
+
+<p><strong>Nom :</strong> {{ $data['name'] }}</p>
+<p><strong>Email :</strong> {{ $data['email'] }}</p>
+
+<p><strong>Sujet :</strong> {{ $data['subject'] }}</p>
+<p>{{ $data['message'] }}</p>
